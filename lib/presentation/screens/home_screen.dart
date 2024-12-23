da3afab209284b6a4f95e7bf542cb7aa667dd85e
Даниил <daniil@MacBook-Pro-Daniil.local>
2024-12-23 22:07:41 +0600

    Переключение ролей. Тест

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,24 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/auth/auth_bloc.dart';
+import '../blocs/auth/auth_event.dart';
+import '../blocs/auth/auth_state.dart';
+import '../../../domain/entities/user.dart' as app_user;
+
 
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      onPopInvokedWithResult: (didPop, result) {
-        if (Navigator.canPop(context)) {
-          Navigator.pop(context);
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        if (state is AuthAuthenticated) {
+          final user = state.user;
+          return PopScope(
+            onPopInvokedWithResult: (didPop, result) {
+              if (Navigator.canPop(context)) {
+                Navigator.pop(context);
+              } else {
+                // Закрыть приложение или добавить логику выхода
+                print("Exiting app");
+              }
+            },
+            child: Scaffold(
+              appBar: AppBar(
+                title: Text('Home'),
+              ),
+              body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Привет ${user.activeRole == app_user.UserRole.performer ? 'Исполнитель' : 'Заказчик'}!'),
+                  SwitchListTile(
+                    title: Text('Переключить роль'),
+                    value: user.activeRole == app_user.UserRole.performer,
+                    onChanged: (value) {
+                      final newRole = value
+                          ? app_user.UserRole.performer
+                          : app_user.UserRole.customer;
+                      context.read<AuthBloc>().add(SwitchRoleEvent(newRole: newRole));
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
         } else {
-          // Закрыть приложение или добавить логику выхода
-          print("Exiting app");
+          return Center(child: CircularProgressIndicator());
         }
       },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
-        ),
-        body: Center(child: Text('Welcome to Home Screen!')),
-      ),
     );
   }
 }
